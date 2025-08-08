64b88d6ee5973197d4098e4880f03c42c6a54317
Jack Tsai <jtsai@nexify.com.hk>
2025-08-08 15:44:27 +0800

    Add log and fix gateway warning

--- account/build.gradle.kts
+++ account/build.gradle.kts
@@ -7,4 +7,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+}
+tasks.named<org.springframework.boot.gradle.tasks.run.BootRun>("bootRun") {
+    jvmArgs = listOf("-Dreactor.netty.http.server.accessLogEnabled=true")
 }
--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -6,14 +6,18 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway:")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation(project(":common"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
 }
 
 dependencyManagement {
     imports {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:2025.0.0")
     }
+}
+tasks.named<org.springframework.boot.gradle.tasks.run.BootRun>("bootRun") {
+    jvmArgs = listOf("-Dreactor.netty.http.server.accessLogEnabled=true")
 }
